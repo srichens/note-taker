6476f81966b3372d3ebb2936aefea2014f31c3a5
Sarah Richens <sarahgrichens@gmail.com>
2023-02-26 15:25:16 -0600

    followed module example to read and write notes

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-let notes = require('./db/db.json');
-const uuid = require('./helpers/uuid');
+const util = require('util'); 
 
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
 const app = express();
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -21,17 +22,34 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+const readFromFile = util.promisify(fs.readFile); 
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  ); 
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
 app.get('/api/notes', (req, res) =>{
-  console.info(`${req.method} request received to view notes`);   
-  res.json(notes)
+  console.info(`${req.method} request received to view notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));   
 });
 
 app.post('/api/notes', (req, res) =>{
   console.info(`${req.method} request received to add a note`); 
-  notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
     
   const { title, text } = req.body;
-
 
   if(title && text){
     const newNote = {
@@ -40,28 +58,12 @@ app.post('/api/notes', (req, res) =>{
       id: uuid(),
     }
 
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    notes.push(newNote);           
-
-  fs.writeFileSync(`./db/db.json`, JSON.stringify(notes), (err) =>
-    err
-      ? console.error(err)
-      : console.log(
-          `New note titled ${newNote.title} has been written to JSON file`
-        )
-  );
-
-    console.log(response);
-    res.status(201).json(response);
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
   } else {
-    res.status(500).json('Error in posting note');
+    res.error('Error in adding note');
   }
 });
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
