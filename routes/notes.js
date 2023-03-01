832e44fa7403c1319dadf9043acc7c7827ce8de4
Sarah Richens <sarahgrichens@gmail.com>
2023-03-01 12:09:34 -0600

    start on delete note

--- routes/notes.js
+++ routes/notes.js
@@ -26,4 +26,20 @@ notes.post('/', (req, res) =>{
   }
 });
 
+notes.delete('/:id', (req, res) =>{
+  console.info(`${req.method} request received to delete a note`);
+    
+  const deleteNote = req.params.id;
+
+  if (req.params.id){
+    res.json(deleteNote); console.log(deleteNote);
+  }
+
+  // readAndAppend(deleteNote, './db/db.json');
+  //   res.json(`Note added successfully 🚀`);
+  // } else {
+  //   res.error('Error in adding note');
+  // }
+});
+
 module.exports = notes;
