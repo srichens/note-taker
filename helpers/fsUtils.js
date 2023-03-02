6ea4cbe7359eef34a72662ba7883aed71ac91b95
Sarah Richens <sarahgrichens@gmail.com>
2023-03-01 19:13:40 -0600

    add comments to javascript

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const util = require('util');
 
+//This is code taken from a class example
+
+// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
+//Function to write data to the JSON file given a destination and some content
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
+//Function to read data from a given a file and append some content
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
--- routes/notes.js
+++ routes/notes.js
@@ -2,11 +2,14 @@ const notes = require('express').Router();
 const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
+//JSON file is read with the promise function and then the data is returned into a javascript object to be used on the front end
 notes.get('/', (req, res) =>{
   console.info(`${req.method} request received to view notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));   
 });
 
+//the JSON file is (synchronously) read, parsed as a javascript array, appended with new note, 
+//written back into JSON file, parsed as a javascript object again, and sent to the front end
 notes.post('/', (req, res) =>{
   console.info(`${req.method} request received to add a note`); 
     
@@ -26,6 +29,10 @@ notes.post('/', (req, res) =>{
   }
 });
 
+//findIndex method ties specified ID to full note after reading and parsing JSON file,
+//index allows selected note to be spliced from note array, and the new array is returned,
+//written back to the JSON file, read again from the file, parsed as a javascript array and sent 
+//as a response to the front end
 notes.delete('/:id', (req, res) =>{
   console.info(`${req.method} request received to delete a note`); 
         
